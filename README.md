727b11e361fca5ff27465b9d22e95bb197639b65
TheQuang <thequang012004@gmail.com>
2024-07-18 07:29:41 +0700

    Update Search Bar and Bookmark Interface

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# appLichSu
-app Lich Su
+# applichsu
+This is an app about Vietnamese history, made by a team of 2 people:
+- Quang : https://github.com/thequang-ntq
+- Thiên : https://github.com/azthienaz/applichsu
+## Getting Started
+Download the Android version of this app through the following link:
+
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
-import 'screens/login_page.dart';
-import 'screens/settings_page.dart';
+// import 'screens/login_page.dart';
+// import 'screens/settings_page.dart';
+import 'screens/bookmark_page.dart';
 
-class HomePage extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+class _HomePageState extends State<HomePage>{
   @override
   Widget build(BuildContext context) {
-    final ThemeData themeData = ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.dark ,
-    );
+    
     return
       Container(
         decoration: const BoxDecoration(
@@ -20,43 +22,75 @@ class HomePage extends StatelessWidget {
                 colors: [Color.fromARGB(255, 228, 160, 59), Colors.white])),
         child: Scaffold(
           backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            title: const Text('Home Page'),
-          ),
           body: Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: SearchAnchor(
-              builder: (BuildContext context, SearchController controller)
-              {
-                return SearchBar(
-                  controller: controller,
-                  padding: const WidgetStatePropertyAll<EdgeInsets>(
-                      EdgeInsets.symmetric(horizontal: 16.0)),
-                  onTap: () {
-                    controller.openView();
-                  },
-                  onChanged: (_) {
-                    controller.openView();
-                  },
-                  leading: const Icon(Icons.search),
-             
-                );
-              },
-              suggestionsBuilder:
-              (BuildContext context, SearchController controller) {
-                return List<ListTile>.generate(5, (int index) {
-                  final String item = 'item $index';
-                  return ListTile(
-                    title: Text(item),
-                    onTap: () {
-                      setState(() {
-                        controller.closeView(item);
-                      });
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+            child: Row(
+              children: [
+                Flexible(
+                  child: SearchAnchor(
+                    viewBackgroundColor: Colors.black,
+                    builder: (BuildContext context, SearchController controller)
+                    {
+                      return SearchBar(
+                        hintText: "Tìm kiếm",
+                        hintStyle: const WidgetStatePropertyAll(
+                          TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w100,
+                            fontStyle: FontStyle.normal,
+                            fontSize: 15,
+                          ),
+                        ),
+                        backgroundColor: const WidgetStatePropertyAll(Colors.black),
+                        controller: controller,
+                        padding: const WidgetStatePropertyAll<EdgeInsets>(
+                            EdgeInsets.symmetric(horizontal: 16.0)),
+                        onTap: () {
+                          controller.openView();
+                        },
+                        onChanged: (_) {
+                          controller.openView();
+                        },
+                        leading: const Icon(Icons.search, color: Colors.white,),
+                   
+                      );
                     },
-                  );
-                });
-              }
+                    suggestionsBuilder:
+                    (BuildContext context, SearchController controller) {
+                      return List<ListTile>.generate(5, (int index) {
+                        final String item = 'item $index';
+                        return ListTile(
+                          title: Text(item),
+                          onTap: () {
+                            setState(() {
+                              controller.closeView(item);
+                            });
+                          },
+                        );
+                      });
+                    }
+                  ),
+                  
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                // Flexible(
+                  IconButton(
+                    color: Colors.white,
+                    iconSize: 50,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const BookmarkPage()),
+                      );
+                    },
+                    icon: const Icon(Icons.bookmark)
+                  ),
+                // ),
+              ],
             ),
+
           ),
         ),
       );
