c4ef53e939b4a007798f5cc1ff9e657043e8f803
TheQuang <thequang012004@gmail.com>
2024-07-19 22:18:07 +0700

    Update Constant Anecdote Page and Costant Museum Page

--- lib/constant_screen/event_page.dart
+++ lib/constant_screen/event_page.dart
@@ -65,82 +65,80 @@ class _ConstantEventPageState extends State<ConstantEventPage>{
           ),
         ),
         body: 
-          Container(
-            child: Column(
-              children: [
-                Image(image: AssetImage('assets/images/event1.png')),
-                const SizedBox(height: 8,),
-                const Text(
-                  "Cuộc khởi nghĩa của Hai Bà Trưng giành thắng lợi",
-                  textAlign: TextAlign.left,
-                  maxLines: 2,
-                  softWrap: true,
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black,
-                  ),
+          Column(
+            children: [
+              const Image(image: AssetImage('assets/images/event1.png')),
+              const SizedBox(height: 8,),
+              const Text(
+                "Cuộc khởi nghĩa của Hai Bà Trưng giành thắng lợi",
+                textAlign: TextAlign.left,
+                maxLines: 2,
+                softWrap: true,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black,
                 ),
-                const SizedBox(height: 5,),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Text(
-                      "40 - 43",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    const SizedBox(width: 40),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 221, 156, 58),
-                        textStyle: const TextStyle(
-                          fontSize: 17,
-                          color: Colors.white,
-                        ),
-                        
-                      ),
-                      child: const Text('Xem video', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500)),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const ConstantVideoPage()),
-                        );
-                      }, 
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 5),
-                const Divider(
-                  color: Colors.grey,
-                  thickness: 2,
-                  height: 16,
-                  indent: 5,
-                  endIndent: 5,
-                ),
-                const SizedBox(height: 5),
-                const Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Text(
-                      'Ngoài ra còn nhiều hiện vật khác như: Ấn đồng Môn hạ Sảnh ấn, thời Tần, niên hiệu Long Khánh thứ 5(1377); bình vẽ thiên nga bằng gốm hoa lam, thời Lê Sơ, thế kỷ 15; bia điện Nam Giao bằng đá, thời Lê Trung hưng, năm Vĩnh Trị 4 (1679), trống Cảnh Thịnh bằng đồng, thời Tây Sơn, niên hiệu Cảnh Thịnh năm 1800, ấn Sắc mệnh chi bảo bằng vàng, thời Nguyễn, năm Minh Mệnh thứ 8 (1827); tập thơ Nhật ký trong tù của Hồ Chí Minh; sách Đường Kách mệnh của Nguyễn Ái Quốc; bản thảo Lời kêu gọi toàn quốc kháng chiến của chủ tịch Hồ Chí Minh',
-                      softWrap: true,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w600,
-                      ),
+              ),
+              const SizedBox(height: 5,),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    "40 - 43",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey,
                     ),
                   ),
+                  const SizedBox(width: 40),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 221, 156, 58),
+                      textStyle: const TextStyle(
+                        fontSize: 17,
+                        color: Colors.white,
+                      ),
+                      
+                    ),
+                    child: const Text('Xem video', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500)),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const ConstantVideoPage()),
+                      );
+                    }, 
+                  ),
+                ],
+              ),
+              const SizedBox(height: 5),
+              const Divider(
+                color: Colors.grey,
+                thickness: 2,
+                height: 16,
+                indent: 5,
+                endIndent: 5,
+              ),
+              const SizedBox(height: 5),
+              const Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    'Ngoài ra còn nhiều hiện vật khác như: Ấn đồng Môn hạ Sảnh ấn, thời Tần, niên hiệu Long Khánh thứ 5(1377); bình vẽ thiên nga bằng gốm hoa lam, thời Lê Sơ, thế kỷ 15; bia điện Nam Giao bằng đá, thời Lê Trung hưng, năm Vĩnh Trị 4 (1679), trống Cảnh Thịnh bằng đồng, thời Tây Sơn, niên hiệu Cảnh Thịnh năm 1800, ấn Sắc mệnh chi bảo bằng vàng, thời Nguyễn, năm Minh Mệnh thứ 8 (1827); tập thơ Nhật ký trong tù của Hồ Chí Minh; sách Đường Kách mệnh của Nguyễn Ái Quốc; bản thảo Lời kêu gọi toàn quốc kháng chiến của chủ tịch Hồ Chí Minh',
+                    softWrap: true,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
                 ),
-              ],
-            ),
-            
+              ),
+            ],
           ),
+            
           bottomNavigationBar: BottomNavigationBar(
               type: BottomNavigationBarType.fixed,
               items: const <BottomNavigationBarItem>[
